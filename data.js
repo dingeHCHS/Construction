2b314fd31cff269159b6c6e6219bf2524991e6d0
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-02-22 11:25:16 -0600

    Update data.js

--- data.js
+++ data.js
@@ -34,7 +34,7 @@ var APP_DATA = {
           "yaw": -1.666876127963528,
           "pitch": -0.05717578907679943,
           "title": "Construction",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1H_xN7zLv0ugzQoVhfbmnwdMzzg_INbjM/preview" width="640" height="480"></iframe>'
         }
       ]
     }
